182d099aa9a901749e5ffecf2bc3752fcdca4161
kunxin-chor <chorkunxin@yahoo.com>
2020-06-04 03:31:21 +0000

    Template inheritance

--- 03-template-inheritance/index.js
+++ 03-template-inheritance/index.js
@@ -14,13 +14,40 @@ app.set('view engine', 'hbs');  // choice of pug, jade, ejs etc. to be our view 
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 
+/* 1B. SETUP HBS */
+hbs.registerHelper("say_hello", function(){
+    return "Hello World"
+})
+
+
+var blocks = {};
+hbs.registerHelper('extend', function(name, context) {
+    var block = blocks[name];
+    if (!block) {
+        block = blocks[name] = [];
+    }
+
+    block.push(context.fn(this)); // for older versions of handlebars, use block.push(context(this));
+});
+
+hbs.registerHelper('block', function(name) {
+    var val = (blocks[name] || []).join('\n');
+
+    // clear the block
+    blocks[name] = [];
+    return val;
+});
+
 /* 2. ROUTES */
 app.get('/', function(req, res){
     res.render('index');
 })
 
 app.get('/greet/:name', function(req,res){
-    res.send("Welcome " + req.params.name);
+    res.render('greet.hbs',{
+        layout: 'layout_bootstrap',
+        name: req.params.name
+    })
 })
 
 app.get('/calc', function(req,res){
